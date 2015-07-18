972cb4179ee09a4829906f9271f8cf53714606c1
vichug <vic.hvk@gmail.com>
2015-07-18 15:51:59 +0200

    added an option to import only used tiles ; includes last commits

--- import.gd
+++ import.gd
@@ -78,7 +78,7 @@ func createTileset(var data, var cell_size, var onlyused, var layers): # data = 
 					ts.create_tile(count)
 					ts.tile_set_texture(count, texture)
 					ts.tile_set_region(count, rect)
-					var id = str(count-1)
+					var id = str(count - int(t["firstgid"]))
 
 					if t.has("tiles"):
 						for tile in tiles:
